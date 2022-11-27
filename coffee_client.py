383cf663ec04d9bcc27c459def094840678cf078
FREEDOM <vinxad@gmail.com>
2022-11-27 17:12:11 +0100

    Order coffee client side

--- coffee_client.py
+++ coffee_client.py
@@ -2,16 +2,37 @@ import socket
 
 
 def UDPClient():
-    server_address = ("127.0.0.1", 3000)
-
+    coffee_machine_server_address = ("192.168.1.167", 3000)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    data = "Hello World"
+    order = b"COFFEE"
+    payment = b"PAYMENT"
 
-    sock.sendto(bytes(data + "\n", "utf-8"), server_address)
-    received = str(sock.recv(1024), "utf-8")
-    print("Sent: {}".format(data))
-    print("Received: {}".format(received))
+    # Order coffee
+    sock.sendto(order, coffee_machine_server_address)
+
+    payload, address = sock.recvfrom(1024)
+    if payload == b"ACCEPTED":
+        print("Coffee machine accepted order - waiting for payment request")
+
+        # Wait for payment request
+        payload, address = sock.recvfrom(1024)
+        if payload == b"PAY":  # Coffee machine requested payment
+            print("Coffee machine requested payment - sending payment")
+            # Send payment
+            sock.sendto(payment, address)
+
+        # Wait for payment confirmation
+        payload, address = sock.recvfrom(1024)
+        if payload == b"CONFIRMED":
+            print("Coffee machine confirmed payment - waiting for coffee")
+
+        # Wait for coffee
+        payload, address = sock.recvfrom(1024)
+        if payload == b"SERVED":
+            print("Coffee machine served coffee - order finished")
+        else:
+            print("Coffee machine did not serve coffee - order failed")
 
 
 def main():
